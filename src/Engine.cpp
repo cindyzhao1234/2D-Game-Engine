bc7d0eb53feb04fbac2cc55b6d59a28d7b614dda
Cindy Zhao <zhaoxin.dy@gmail.com>
2026-03-12 01:41:23 +1100

    removed test code from engine.cpp

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -28,29 +28,6 @@ bool Engine::init(int w, int h, const char* title){
 }
 
 void Engine::run(){
-    //-----TEST CODE
-    World world;
-
-    Entity background = world.createEntity();
-    world.addSprite(background, {"background", 0});
-    world.addTransform(background, {{100.0, 100.0}, 0.0, {1.0, 1.0}});
-    Entity player = world.createEntity();
-    world.addSprite(player, {"player", 10});
-    world.addTransform(player, {{100.0, 100.0}, 0.0, {1.0, 1.0}});
-
-    Entity NPC = world.createEntity();
-    world.addSprite(NPC, {"NPC", 10});
-    world.addTransform(NPC, {{200.0, 200.0}, 0.0, {1.0, 1.0}});
-
-    MyAssetManager assetManager;
-
-    assetManager.loadTexture("background", "../background.png");
-    assetManager.loadTexture("player", "../player.png");
-    assetManager.loadTexture("NPC", "../kettleTest.png");
-
-    Input input;
-    MyRenderer2D renderer;
-    //--- END TEST CODE
 
     while(!WindowShouldClose() && running){
         //update dt using raylib's frame time
@@ -60,21 +37,6 @@ void Engine::run(){
         ClearBackground(WHITE);
         DrawFPS(10,10);
         
-        //----------TEST CODE
-        
-        float speed = 300.0f;
-
-        movementSystem(world, player, input, dt, speed);
-        renderer.beginFrame();
-
-        renderSystem(world, renderer, assetManager);
-
-        renderer.flush();
-
-
-        // assetManager.unloadAll();
-        // ------- END TEST CODE
-
         EndDrawing();
     }
 }
